a20e65634785e8887ba68b3f586c8e9be0eba186
bnshivadattakumara <bnshivadattakumara@gmail.com>
2026-08-16 04:43:00 +0530

    Fix Cloudflare OpenNext deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
   images: {
     unoptimized: true,
   },
@@ -16,3 +15,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
